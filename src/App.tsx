0dc407d819b8af02cdbba2f4f6b6563212386be3
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-03-26 17:43:44 -0300

    chore: create a grid wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import GridWrapper from './components/Grid';
+
 import Routes from './routes';
 import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/global';
@@ -8,8 +10,10 @@ import { GlobalStyle } from './styles/global';
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Routes />
+      <GridWrapper>
+        <GlobalStyle />
+        <Routes />
+      </GridWrapper>
     </ThemeProvider>
   );
 }
---
+++ src/components/Grid/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Grid } from './styles';
+
+interface IGridProps {
+  children: React.ReactNode
+}
+
+export default function GridWrapper({ children }: IGridProps) {
+  return (
+    <Container>
+      <Grid>
+        {children}
+      </Grid>
+    </Container>
+  )
+}
