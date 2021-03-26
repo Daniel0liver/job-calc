827ee9cdeafbfbd87a86ffc8a4d0d4d4742e0d59
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-03-26 11:20:54 -0300

    chore: create initial structure of the project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>JobCalc</p>
+      </header>
+      <main>
+        <p>Hello word</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
