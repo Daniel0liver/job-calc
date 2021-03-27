c311d278f5a65543bec5cd71edce3399d1b2be72
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-03-27 10:23:57 -0300

    chore create a header of the app

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import AlertIcon from 'assets/images/alert-octagon.svg';
+import LogoIcon from 'assets/images/logo.svg';
+import ProfileIcon from 'assets/images/profile.png';
+
+import GridWrapper from 'components/Grid';
+
+import {
+  Header,
+  Container,
+  LeftWrapper,
+  CenterWrapper,
+  RightWrapper,
+  Alert,
+  ImageProfile,
+  UserName,
+  Subtitle,
+  LinkWrapper,
+  LabelWrapper
+} from './styles';
+
+export default function CustomHeader() {
+  return (
+    <Header>
+      <GridWrapper>
+        <Container>
+          <LeftWrapper>
+            <img src={LogoIcon} alt="Logo principal" />
+          </LeftWrapper>
+          <CenterWrapper>
+            <img src={AlertIcon} alt="Icone alerta" />
+            <Alert>Você tem 2 horas livres no seu dia</Alert>
+          </CenterWrapper>
+          <RightWrapper>
+            <LinkWrapper>
+              <LabelWrapper>
+                <UserName>Jaqueline</UserName>
+                <Subtitle>Ver perfil</Subtitle>
+              </LabelWrapper>
+              <ImageProfile src={ProfileIcon} />
+            </LinkWrapper>
+          </RightWrapper>
+        </Container>
+      </GridWrapper>
+    </Header>
+  );
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,89 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  height: 10vh;
+  background: ${({ theme }) => theme.palette.primary.main};
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 10vh;
+`;
+
+export const LeftWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const CenterWrapper = styled.div`
+  flex: 2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RightWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const Alert = styled.h6`
+  margin-left: 0.5rem;
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 15px;
+  color: ${({ theme }) => theme.palette.primary.text.p};
+`;
+
+export const LabelWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  margin-right: 1rem;
+`;
+
+export const ImageProfile = styled.img`
+  width: 3.5rem;
+  height: 3.5rem;
+  border-radius: 50%;
+  border: solid 2px ${({ theme }) => theme.palette.secondary.main};
+`;
+
+export const UserName = styled.h2`
+  text-align: right;
+  font-size: 16px;
+  color: ${({ theme }) => theme.palette.primary.text.p};
+`;
+
+export const Subtitle = styled.h6`
+  margin-top: 0.4rem;
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 13px;
+  color: ${({ theme }) => theme.palette.primary.text.p};
+`;
+
+export const LinkWrapper = styled.a`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4rem;
+  padding: 0.2rem 0.3rem 0.2rem 2rem;
+
+  &:hover {
+    background-color: #53535fed;
+    cursor: pointer;
+
+    ${Subtitle} {
+      color: ${({ theme }) => theme.palette.secondary.main};
+    }
+  }
+`;
